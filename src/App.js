632532e9396aa7b9d7a10dfb5eee1397afa188e7
dani <groddpapyrus@yahoo.com>
2019-08-07 08:28:46 +0500

    react state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { tsImportEqualsDeclaration } from '@babel/types';
 
 class App extends Component {
 
@@ -15,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.name}
+        <Person name={this.state.name} age={this.props.age} />
       </div>
     )
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Person = () => {
-    
+const Person = (props) => {
+    <p>I am {props.name} and my age is {props.age}</p>
 }
